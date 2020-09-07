9efdc88fad4dea6b6c905bdc2ce3e9a93cdb42ee
Yuxd <349434222@qq.com>
2020-09-07 23:02:22 +0800

    1.http请求提交

--- app/src/main/java/com/xiaobao/good/InitApplication.java
+++ app/src/main/java/com/xiaobao/good/InitApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.xiaobao.good.db.AbstractAppDatabase;
+import com.xiaobao.good.http.HttpUtils;
 
 public class InitApplication extends Application {
     private static Context context;
@@ -13,6 +14,7 @@ public class InitApplication extends Application {
         super.onCreate();
         context = this.getApplicationContext();
         AbstractAppDatabase.initDb(context);
+        HttpUtils.initHttp();
     }
 
     public static Context getContext() {
---
+++ app/src/main/java/com/xiaobao/good/http/bean/ClientBean.java
@@ -0,0 +1,256 @@
+package com.xiaobao.good.http.bean;
+
+public class ClientBean {
+
+    /**
+     * age : 37
+     * client_address : 上海市宝山区爱辉路
+     * client_area : null
+     * client_birthday : 429465600000
+     * client_car : 是
+     * client_cname : null
+     * client_email : null
+     * client_house : 是
+     * client_id : 31
+     * client_idcard : 15563862562839
+     * client_income : 30
+     * client_job : 博主
+     * client_keep : null
+     * client_label : 旅游
+     * client_marriage : 已婚
+     * client_name : 李斯
+     * client_origin : null
+     * client_phone : 18655377368
+     * client_sex : 男
+     * client_type : P
+     * employee_id : 4
+     * employee_name : 员工1
+     */
+
+    private int age;
+    private String client_address;
+    private Object client_area;
+    private long client_birthday;
+    private String client_car;
+    private Object client_cname;
+    private Object client_email;
+    private String client_house;
+    private int client_id;
+    private String client_idcard;
+    private int client_income;
+    private String client_job;
+    private Object client_keep;
+    private String client_label;
+    private String client_marriage;
+    private String client_name;
+    private Object client_origin;
+    private String client_phone;
+    private String client_sex;
+    private String client_type;
+    private int employee_id;
+    private String employee_name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getClient_address() {
+        return client_address;
+    }
+
+    public void setClient_address(String client_address) {
+        this.client_address = client_address;
+    }
+
+    public Object getClient_area() {
+        return client_area;
+    }
+
+    public void setClient_area(Object client_area) {
+        this.client_area = client_area;
+    }
+
+    public long getClient_birthday() {
+        return client_birthday;
+    }
+
+    public void setClient_birthday(long client_birthday) {
+        this.client_birthday = client_birthday;
+    }
+
+    public String getClient_car() {
+        return client_car;
+    }
+
+    public void setClient_car(String client_car) {
+        this.client_car = client_car;
+    }
+
+    public Object getClient_cname() {
+        return client_cname;
+    }
+
+    public void setClient_cname(Object client_cname) {
+        this.client_cname = client_cname;
+    }
+
+    public Object getClient_email() {
+        return client_email;
+    }
+
+    public void setClient_email(Object client_email) {
+        this.client_email = client_email;
+    }
+
+    public String getClient_house() {
+        return client_house;
+    }
+
+    public void setClient_house(String client_house) {
+        this.client_house = client_house;
+    }
+
+    public int getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(int client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_idcard() {
+        return client_idcard;
+    }
+
+    public void setClient_idcard(String client_idcard) {
+        this.client_idcard = client_idcard;
+    }
+
+    public int getClient_income() {
+        return client_income;
+    }
+
+    public void setClient_income(int client_income) {
+        this.client_income = client_income;
+    }
+
+    public String getClient_job() {
+        return client_job;
+    }
+
+    public void setClient_job(String client_job) {
+        this.client_job = client_job;
+    }
+
+    public Object getClient_keep() {
+        return client_keep;
+    }
+
+    public void setClient_keep(Object client_keep) {
+        this.client_keep = client_keep;
+    }
+
+    public String getClient_label() {
+        return client_label;
+    }
+
+    public void setClient_label(String client_label) {
+        this.client_label = client_label;
+    }
+
+    public String getClient_marriage() {
+        return client_marriage;
+    }
+
+    public void setClient_marriage(String client_marriage) {
+        this.client_marriage = client_marriage;
+    }
+
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+    public Object getClient_origin() {
+        return client_origin;
+    }
+
+    public void setClient_origin(Object client_origin) {
+        this.client_origin = client_origin;
+    }
+
+    public String getClient_phone() {
+        return client_phone;
+    }
+
+    public void setClient_phone(String client_phone) {
+        this.client_phone = client_phone;
+    }
+
+    public String getClient_sex() {
+        return client_sex;
+    }
+
+    public void setClient_sex(String client_sex) {
+        this.client_sex = client_sex;
+    }
+
+    public String getClient_type() {
+        return client_type;
+    }
+
+    public void setClient_type(String client_type) {
+        this.client_type = client_type;
+    }
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getEmployee_name() {
+        return employee_name;
+    }
+
+    public void setEmployee_name(String employee_name) {
+        this.employee_name = employee_name;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientBean{" +
+                "age=" + age +
+                ", client_address='" + client_address + '\'' +
+                ", client_area=" + client_area +
+                ", client_birthday=" + client_birthday +
+                ", client_car='" + client_car + '\'' +
+                ", client_cname=" + client_cname +
+                ", client_email=" + client_email +
+                ", client_house='" + client_house + '\'' +
+                ", client_id=" + client_id +
+                ", client_idcard='" + client_idcard + '\'' +
+                ", client_income=" + client_income +
+                ", client_job='" + client_job + '\'' +
+                ", client_keep=" + client_keep +
+                ", client_label='" + client_label + '\'' +
+                ", client_marriage='" + client_marriage + '\'' +
+                ", client_name='" + client_name + '\'' +
+                ", client_origin=" + client_origin +
+                ", client_phone='" + client_phone + '\'' +
+                ", client_sex='" + client_sex + '\'' +
+                ", client_type='" + client_type + '\'' +
+                ", employee_id=" + employee_id +
+                ", employee_name='" + employee_name + '\'' +
+                '}';
+    }
+}
