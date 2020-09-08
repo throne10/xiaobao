250cca4d883bf0d6bdac6051b05e580c127a2a5e
Yuxd <349434222@qq.com>
2020-09-08 23:03:29 +0800

    1.行程安排提交，微信记录提交

--- app/src/main/java/com/xiaobao/good/retrofit/interfaces/HttpInterface.java
+++ app/src/main/java/com/xiaobao/good/retrofit/interfaces/HttpInterface.java
@@ -5,10 +5,13 @@ import com.xiaobao.good.retrofit.result.UserInfoData;
 import com.xiaobao.good.retrofit.result.WechatRecord;
 
 import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Query;
 
 public interface HttpInterface {
