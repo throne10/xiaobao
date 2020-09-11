00473fae896d537fb6d33b78db08b3f4e57e57af
Muyunfan <964013025@qq.com>
2020-09-12 00:27:46 +0800

    登录之后跳转到主界面

--- app/src/main/java/com/xiaobao/good/LoginActivity.java
+++ app/src/main/java/com/xiaobao/good/LoginActivity.java
@@ -1,13 +1,12 @@
 package com.xiaobao.good;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.xiaobao.good.common.CommonUtils;
 import com.xiaobao.good.common.Constants;
@@ -20,6 +19,7 @@ import com.xiaobao.good.sp.UserSp;
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.appcompat.app.AppCompatActivity;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,7 +29,6 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "xiaobao_login";
 
     enum Result {
-
         SUCCESS(""),
         FAILVALID("用户名或者密码不可为空"),
         FAILNETWORKFAIL("网络未连接");
@@ -49,7 +48,6 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,27 +57,24 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = findViewById(R.id.tv_pwd);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        loginButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-                String name = usernameEditText.getText().toString().trim();
-                String pwd = passwordEditText.getText().toString().trim();
+                        String name = usernameEditText.getText().toString().trim();
+                        String pwd = passwordEditText.getText().toString().trim();
 
-                Result re = conditionCheck(name, pwd);
+                        Result re = conditionCheck(name, pwd);
 
-                if (!Result.SUCCESS.equals(re)) {
-                    toast(re.getMsg());
-                } else if (Result.SUCCESS.equals(re)) {
+                        if (!Result.SUCCESS.equals(re)) {
+                            toast(re.getMsg());
+                        } else if (Result.SUCCESS.equals(re)) {
 
-                    doLogin(name, pwd);
-
-                }
-
-
-            }
-        });
-
+                            doLogin(name, pwd);
+                        }
+                    }
+                });
     }
 
     private void doLogin(String name, String pwd) {
@@ -89,56 +84,70 @@ public class LoginActivity extends AppCompatActivity {
             map.put("loginName", name);
             map.put("loginPwd", pwd);
             Call<UserInfoData> call = RetrofitUtils.getService().login(name, pwd);
-            call.enqueue(new Callback<UserInfoData>() {
-                @Override
-                public void onResponse(Call<UserInfoData> call, Response<UserInfoData> response) {
-
-                    LogUtil.i(TAG, "re:" + response.isSuccessful() + "," + response.message() + ">>" + response.body());
-
-                    UserInfoData userInfoData = response.body();
-
-
-                    if (userInfoData != null) {
-                        if (Constants.LOGIN_SUCC.equals(userInfoData.getCode())) {
+            call.enqueue(
+                    new Callback<UserInfoData>() {
+                        @Override
+                        public void onResponse(
+                                Call<UserInfoData> call, Response<UserInfoData> response) {
 
                             try {
-                                /**
-                                 * 登录成功。保存到本地
-                                 */
-                                UserSp.getInstances().saveUser(userInfoData.getData());
-
-                                UserInfoData.LoginUserData loginUserData = UserSp.getInstances().getUser();
-                                LogUtil.i(TAG, "read from sp:" + loginUserData.toString());
-
+                                LogUtil.i(
+                                        TAG,
+                                        "re:"
+                                                + response.isSuccessful()
+                                                + ","
+                                                + response.message()
+                                                + ">>"
+                                                + response.body());
                             } catch (Exception e) {
-                                LogUtil.e(TAG, "read from sp e:" + e);
+                                LogUtil.e(TAG, "login e > " + e.toString());
                             }
 
+                            if (response.isSuccessful()) {
+                                UserInfoData userInfoData = response.body();
+
+                                if (userInfoData != null) {
+                                    if (Constants.LOGIN_SUCC.equals(userInfoData.getCode())) {
+
+                                        try {
+                                            /** 登录成功。保存到本地 */
+                                            UserSp.getInstances().saveUser(userInfoData.getData());
+
+                                            UserInfoData.LoginUserData loginUserData =
+                                                    UserSp.getInstances().getUser();
+                                            LogUtil.i(
+                                                    TAG,
+                                                    "read from sp:" + loginUserData.toString());
+
+                                            Intent intent =
+                                                    new Intent(
+                                                            LoginActivity.this, BaseActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            startActivity(intent);
+                                            finish();
+                                        } catch (Exception e) {
+                                            LogUtil.e(TAG, "read from sp e:" + e);
+                                        }
+                                    }
+                                }
+                            }
                         }
-                    }
 
+                        @Override
+                        public void onFailure(Call<UserInfoData> call, Throwable t) {
 
-                }
-
-                @Override
-                public void onFailure(Call<UserInfoData> call, Throwable t) {
-
-                    LogUtil.i(TAG, "fail re:" + t.getMessage());
-
-                }
-            });
+                            LogUtil.i(TAG, "fail re:" + t.getMessage());
+                        }
+                    });
         } catch (Exception e) {
             LogUtil.i(TAG, "e:" + e);
         }
-
-
     }
 
     private Result conditionCheck(String name, String pwd) {
 
         if (StringUtils.isEmpty(name) || StringUtils.isEmpty(pwd)) {
             return Result.FAILVALID;
-
         }
 
         if (!CommonUtils.isNetWorkAvail(this)) {
@@ -147,10 +156,8 @@ public class LoginActivity extends AppCompatActivity {
         return Result.SUCCESS;
     }
 
-
     private void toast(String msg) {
 
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
-
 }
--- app/src/main/java/com/xiaobao/good/ui/ClientViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.xiaobao.good.ui;
-
-import androidx.lifecycle.ViewModel;
-
-public class ClientViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
